a9b2ef2fd391140430795da015f1fb9a3d308d8b
Olami007 <oladimejibells@gmail.com>
2023-07-10 22:27:24 +0100

    data fetched with swr

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
 import Button from "@/components/button/Button";
+import { items } from "./data.js";
+import { notFound } from "next/navigation";
+
+const getData = (category) => {
+  const data = items[category];
+
+  if (data) {
+    return data;
+  }
+  return notFound();
+};
 
 const Category = ({ params }) => {
+  const data = getData(params.category);
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
 
-      {/* {data.map((item) => (
+      {data.map((item) => (
         <div className={styles.item} key={item.id}>
           <div className={styles.content}>
             <h1 className={styles.title}>{item.title}</h1>
@@ -19,7 +31,7 @@ const Category = ({ params }) => {
             <Image className={styles.img} fill={true} src={item.image} alt="" />
           </div>
         </div>
-      ))} */}
+      ))}
     </div>
   );
 };
